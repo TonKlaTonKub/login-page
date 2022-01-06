2e6d758e0d1e3f21d0c299ce604a031e6de696af
TonKlaTonKub <phetkla1992@gmail.com>
2022-01-07 00:54:35 +0700

    Logout Function

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SignInData } from '../model/signInData';
+import { AuthenticationService } from '../service/authentication/authentication.service';
+
+@Component({
+  selector: 'cf-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authenticationService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(signInForm: NgForm){
+    console.log(signInForm.value);
+    const signInData = new SignInData(signInForm.value.email, signInForm.value.password);
+    this.authenticationService.authenticate(signInData);
+  }
+}
